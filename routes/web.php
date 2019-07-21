6a122d210e8e440131b87e5a8b814473f6abc9c4
Ekaterina Budon <ekaterinabudon@gmail.com>
2019-07-21 12:18:05 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,27 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
-$router->get('/hello', function () use ($router) {
-    return '<h1>Hello!</h1>';
-});
-$router->get('/sum/{n1}/{n2}', function ($n1, $n2) {
-    return '<h1>Сумма:</h1><h2>' . ($n1+$n2) . '</h2>';
-});
-
+  $router->get('/', function () use ($router) {
+    date_default_timezone_set('Europe/Moscow');
+      $getDate = new class { function __construct($format = 'j/m/y H:i') {
+        $this->format = $format;
+      }
+      function getD() {
+        return date($this->format);
+      }
+    };
+      
+    $date = $getDate->getD();
+      
+    echo $date;
+      
+  });
+  $router->get('/print', function () use ($router) {
+    header('Content-type: text/plain; charset=utf-8');
+    echo file_get_contents(__FILE__);
+  });
+    $router->get('/print&public', function () use ($router) {
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: text/plain; charset=utf-8');
+    header('Access-Control-Allow-Methods: GET, POST, DELETE');
+    header('Content-type: text/plain; charset=utf-8');
+    echo file_get_contents(__FILE__);
+  });
